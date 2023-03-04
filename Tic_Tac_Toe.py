b7a9196288165655e7c0939be587a2c572152b9e
vmatevicius <vytis765@gmail.com>
2023-03-04 19:35:14 +0200

    Added inheritance and polymorphism examples

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -8,8 +8,6 @@ logging.basicConfig(level=logging.DEBUG,filename='data.log', filemode='a', forma
 
 class Game:
 
-    GAME_STATUS = True
-    
     game_board = ["[ ]","[ ]","[ ]",
                   "[ ]","[ ]","[ ]",
                   "[ ]","[ ]","[ ]"]
@@ -20,8 +18,8 @@ class Game:
             raise TypeError("Name must be a string")
         self.player_one: str = player_one_name
         self.player_two: str = player_two_name
-        self.player_one_sign: str = ""
-        self.player_two_sign: str = ""
+        self.__player_one_sign: str = ""
+        self.__player_two_sign: str = ""
         logging.info(f"Game succesfully inicialized between players {self.player_one} and {self.player_two}!")
     
     def show_board(self) -> None:
@@ -85,7 +83,7 @@ class Game:
             
             self.show_board()
 
-            while self.GAME_STATUS:
+            while True:
                 
                 location = int(input(f"{self.player_one} turn, choose a valid location: "))
                 
@@ -108,8 +106,8 @@ class Game:
                     
                     if turns == 9:
                         print("No one won!")
-                        self.GAME_STATUS = False
                         logging.info("Game succesfully ended without a winner")
+                        break
                     
                 location = int(input(f"{self.player_two} turn, choose a valid location: "))
                 
@@ -132,13 +130,13 @@ class Game:
                     
                     if turns == 9:
                         print("No one won!")
-                        self.GAME_STATUS = False
                         logging.info("Game succesfully ended without a winner")
+                        break
         else:
             
             self.show_board()
             
-            while self.GAME_STATUS:
+            while True:
 
                 location = int(input(f"{self.player_two} turn, choose a valid location: "))
                 
@@ -161,8 +159,8 @@ class Game:
                     
                     if turns == 9:
                         print("No one won!")
-                        self.GAME_STATUS = False
                         logging.info("Game succesfully ended without a winner")
+                        break
                     
                 location = int(input(f"{self.player_one} turn, choose a valid location: "))
                 
@@ -185,8 +183,8 @@ class Game:
                     
                     if turns == 9:  
                         print("No one won!")
-                        self.GAME_STATUS = False
                         logging.info("Game succesfully ended without a winner")
+                        break
                 
     def check_if_winner_exists(self) -> bool:
         
@@ -292,3 +290,166 @@ class Game:
         self.assign_signs_to_players()
         print(f"{self.player_one} is {self.player_one_sign} and {self.player_two} is {self.player_two_sign}")
         self.start_match()
+        
+class PlusMinus(Game):
+    
+    def __init__(self, player_one_name: str, player_two_name: str) -> None:
+         super().__init__(player_one_name, player_two_name)
+    
+    
+    def assign_signs_to_players(self) -> str:
+        
+        print("\n")
+        print("Before starting the game")
+        print("Players must choose between + and - signs")
+        
+        if randint(1,2) == 1:
+            
+            while True:
+            
+                self.player_one_sign = input(f"{self.player_one} chooses: ").strip()
+                
+                if self.player_one_sign not in ["+", "-"]:
+                    print("Must choose between + and -!!")
+                    continue
+                else:
+                    break
+                
+            if self.player_one_sign == "+":   
+                print(f"{self.player_two} is left with - sign")
+                self.player_two_sign = "-"
+                    
+            else:
+                print(f"{self.player_two} is left with + sign")
+                self.player_two_sign = "+"
+
+        else:
+            
+            while True:
+                
+                self.player_two_sign = input(f"{self.player_two} chooses: ").strip().upper()
+                    
+                if self.player_two_sign not in ["+", "-"]:
+                    print("Must choose between + and -!!")
+                    continue
+                else:
+                    break
+                
+            if self.player_two_sign == "+":
+                print(f"{self.player_one} is left with - sign")
+                self.player_one_sign = "-"
+                    
+            else:
+                print(f"{self.player_one} is left with + sign")
+                self.player_one_sign = "+"
+    
+    def start_match(self) -> None:
+        
+        turns = 0
+        
+        if self.player_one_sign == "+":
+            
+            self.show_board()
+
+            while True:
+                
+                location = int(input(f"{self.player_one} turn, choose a valid location: "))
+                
+                if location not in [0,1,2,3,4,5,6,7,8]:
+                    print("Number must be between 0 and 8")
+                    continue
+                
+                elif self.game_board[location] != "[ ]":
+                    print("Choose only empty squares!!!")
+                    continue
+                
+                else:
+                    self.game_board[location] = "[+]"
+                    turns += 1
+                    self.show_board()
+                    
+                    if self.check_if_winner_exists():
+                        logging.info("Game succesfully ended with a winner")
+                        break
+                    
+                    if turns == 9:
+                        print("No one won!")
+                        logging.info("Game succesfully ended without a winner")
+                        break
+                    
+                location = int(input(f"{self.player_two} turn, choose a valid location: "))
+                
+                if location not in [0,1,2,3,4,5,6,7,8]:
+                    print("Number must be between 0 and 8")
+                    continue
+                
+                elif self.game_board[location] != "[ ]":
+                    print("Choose only empty squares!!!")
+                    continue
+                
+                else:
+                    self.game_board[location] = "[-]"
+                    turns += 1
+                    self.show_board()
+                    
+                    if self.check_if_winner_exists():
+                        logging.info("Game succesfully ended with a winner")
+                        break
+                    
+                    if turns == 9:
+                        print("No one won!")
+                        logging.info("Game succesfully ended without a winner")
+                        break
+        else:
+            
+            self.show_board()
+            
+            while True:
+
+                location = int(input(f"{self.player_two} turn, choose a valid location: "))
+                
+                if location not in [0,1,2,3,4,5,6,7,8]:
+                    print("Number must be between 0 and 8")
+                    continue
+                
+                elif self.game_board[location] != "[ ]":
+                    print("Choose only empty squares!!!")
+                    continue
+                
+                else:
+                    self.game_board[location] = "[+]"
+                    turns += 1
+                    self.show_board()
+                    
+                    if self.check_if_winner_exists():
+                        logging.info("Game succesfully ended with a winner")
+                        break
+                    
+                    if turns == 9:
+                        print("No one won!")
+                        logging.info("Game succesfully ended without a winner")
+                        break
+                    
+                location = int(input(f"{self.player_one} turn, choose a valid location: "))
+                
+                if location not in [0,1,2,3,4,5,6,7,8]: 
+                    print("Number must be between 0 and 8")
+                    continue
+                
+                elif self.game_board[location] != "[ ]":
+                    print("Choose only empty squares!!!")
+                    continue
+                
+                else:
+                    self.game_board[location] = "[-]"
+                    turns += 1
+                    self.show_board()
+                    
+                    if self.check_if_winner_exists():
+                        logging.info("Game succesfully ended with a winner")
+                        break
+                    
+                    if turns == 9:  
+                        print("No one won!")
+                        logging.info("Game succesfully ended without a winner")
+                        break
