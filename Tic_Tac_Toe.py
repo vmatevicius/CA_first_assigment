704346050b70ccd5150e4c9cb940d301c1a3e16c
vmatevicius <vytis765@gmail.com>
2023-03-04 18:45:04 +0200

    Pushing almost complete game of tic tac toe

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,27 +1,281 @@
-from game_helpers import show_board, introduction
-
-# initialize names for players
-# create a board for the game
-# at the start of the game initialize the board
-# let the user choose whether he wants to be X or O
-# make a function thats asks user where he wants to put X or O, then checks if the spot is empty,if empty put the sign in the spot if not asks for the input again
-# then after 3 moves are made the program starts checking if 3 signs connect Vertically/Horizontally/Diagonally if not asks the user for the input again if yes then return the winner
-# When 9 moves are made and there is no winner programs shuts and declares draw between opponents
+from game_helpers import introduction
+from random import randint
+import logging
 
 class Game:
 
-    def __init__(self, player_one_name: str, player_two_name: str):
+    GAME_STATUS = True
+    
+    game_board = ["[ ]","[ ]","[ ]",
+                  "[ ]","[ ]","[ ]",
+                  "[ ]","[ ]","[ ]"]
+    
+    def __init__(self, player_one_name: str, player_two_name: str) -> None:
+        if not player_one_name:
+            raise ValueError("First player name is missing")
         self.player_one: str = player_one_name
+        if not player_two_name:
+            raise ValueError("Second player name is missing")
         self.player_two: str = player_two_name
-        
+        self.player_one_sign: str = ""
+        self.player_two_sign: str = ""
+        logging.info(f"Game succesfully inicialized between player {self.player_one} and {self.player_two}!")
+    
+    def show_board(self) -> None:
+
+        print(self.game_board[0] + "|" + self.game_board[1] + "|" + self.game_board[2] + "      " +  "0|1|2")
+        print(self.game_board[3] + "|" + self.game_board[4] + "|" + self.game_board[5] + "      " +  "3|4|5")
+        print(self.game_board[6] + "|" + self.game_board[7] + "|" + self.game_board[8] + "      " +  "6|7|8")
+    
     def assign_signs_to_players(self) -> str:
+        
+        print("Before starting the game")
         print("Players must choose between X and O signs")
-        player_one_sign = input(f"{self.player_one} chooses: ").strip().upper
-        player_two_sign = ""
-        if player_one_sign == "X":
-            print(f"{self.player_two} is left with O sign")
-            player_two_sign == "O"
-            return player_one_sign, player_two_sign
-        print(f"{self.player_two} is left with X sign")
-        player_two_sign == "X"
-        return player_one_sign, player_two_sign 
+        
+        if randint(1,2) == 1:
+            
+            while True:
+            
+                self.player_one_sign = input(f"{self.player_one} chooses: ").strip().upper()
+                
+                if self.player_one_sign not in ["X", "O"]:
+                    print("Must choose between X and O!!")
+                    continue
+                else:
+                    break
+                
+            if self.player_one_sign == "X":   
+                print(f"{self.player_two} is left with O sign")
+                self.player_two_sign = "O"
+                    
+            else:
+                print(f"{self.player_two} is left with X sign")
+                self.player_two_sign = "X"
+
+        else:
+            
+            while True:
+                
+                self.player_two_sign = input(f"{self.player_two} chooses: ").strip().upper()
+                    
+                if self.player_two_sign not in ["X", "O"]:
+                    print("Must choose between X and O!!")
+                    continue
+                else:
+                    break
+                
+            if self.player_two_sign == "X":
+                print(f"{self.player_one} is left with O sign")
+                self.player_one_sign = "O"
+                    
+            else:
+                print(f"{self.player_one} is left with X sign")
+                self.player_one_sign = "X"
+
+        
+    def start_match(self) -> None:
+        
+        turns = 0
+        
+        if self.player_one_sign == "X":
+
+            while self.GAME_STATUS:
+            
+                location = int(input(f"{self.player_one} turn, choose a valid location: "))
+                
+                if location not in [0,1,2,3,4,5,6,7,8]:
+                    print("Number must be between 0 and 8")
+                    continue
+                
+                elif self.game_board[location] != "[ ]":
+                    print("Choose only empty squares!!!")
+                    continue
+                
+                else:
+                    self.game_board[location] = "[X]"
+                    turns += 1
+                    self.show_board()
+                    
+                    if self.check_if_winner_exists():
+                        logging.info("Game succesfully ended with a winner")
+                        break
+                    
+                    if turns == 9:
+                        print("No one won!")
+                        self.GAME_STATUS = False
+                        logging.info("Game succesfully ended without a winner")
+                    
+                location = int(input(f"{self.player_two} turn, choose a valid location: "))
+                
+                if location not in [0,1,2,3,4,5,6,7,8]:
+                    print("Number must be between 0 and 8")
+                    continue
+                
+                elif self.game_board[location] != "[ ]":
+                    print("Choose only empty squares!!!")
+                    continue
+                
+                else:
+                    self.game_board[location] = "[O]"
+                    turns += 1
+                    self.show_board()
+                    
+                    if self.check_if_winner_exists():
+                        logging.info("Game succesfully ended with a winner")
+                        break
+                    
+                    if turns == 9:
+                        print("No one won!")
+                        self.GAME_STATUS = False
+                        logging.info("Game succesfully ended without a winner")
+        else:
+            
+            while self.GAME_STATUS:
+                
+                location = int(input(f"{self.player_two} turn, choose a valid location: "))
+                
+                if location not in [0,1,2,3,4,5,6,7,8]:
+                    print("Number must be between 0 and 8")
+                    continue
+                
+                elif self.game_board[location] != "[ ]":
+                    print("Choose only empty squares!!!")
+                    continue
+                
+                else:
+                    self.game_board[location] = "[X]"
+                    turns += 1
+                    self.show_board()
+                    
+                    if self.check_if_winner_exists():
+                        logging.info("Game succesfully ended with a winner")
+                        break
+                    
+                    if turns == 9:
+                        print("No one won!")
+                        self.GAME_STATUS = False
+                        logging.info("Game succesfully ended without a winner")
+                    
+                location = int(input(f"{self.player_one} turn, choose a valid location: "))
+                
+                if location not in [0,1,2,3,4,5,6,7,8]: 
+                    print("Number must be between 0 and 8")
+                    continue
+                
+                elif self.game_board[location] != "[ ]":
+                    print("Choose only empty squares!!!")
+                    continue
+                
+                else:
+                    self.game_board[location] = "[O]"
+                    turns += 1
+                    self.show_board()
+                    
+                    if self.check_if_winner_exists():
+                        logging.info("Game succesfully ended with a winner")
+                        break
+                    
+                    if turns == 9:  
+                        print("No one won!")
+                        self.GAME_STATUS = False
+                        logging.info("Game succesfully ended without a winner")
+                
+    def check_if_winner_exists(self) -> bool:
+        
+        if self.game_board[0] == self.game_board[1] == self.game_board[2] != "[ ]":
+            
+            if self.game_board[0].replace('[','').replace(']','') == self.player_one_sign:
+                print(f"{self.player_one} Wins!!")
+                
+            else:
+                print(f"{self.player_two} Wins!!")
+                
+            return True
+            
+        elif self.game_board[3] == self.game_board[4] == self.game_board[5] != "[ ]":
+            
+            if self.game_board[3].replace('[','').replace(']','') == self.player_one_sign:
+                print(f"{self.player_one} Wins!!")
+                
+            else:
+                print(f"{self.player_two} Wins!!")
+                
+            return True
+            
+        elif self.game_board[6] == self.game_board[7] == self.game_board[8] != "[ ]":
+            
+            if self.game_board[6].replace('[','').replace(']','') == self.player_one_sign:
+                print(f"{self.player_one} Wins!!")
+                
+            else:
+                print(f"{self.player_two} Wins!!")
+                
+            return True
+            
+        elif self.game_board[0] == self.game_board[3] == self.game_board[6] != "[ ]":
+            
+            if self.game_board[0].replace('[','').replace(']','') == self.player_one_sign:
+                print(f"{self.player_one} Wins!!")
+                
+            else:
+                print(f"{self.player_two} Wins!!")
+                
+            return True
+            
+        elif self.game_board[1] == self.game_board[4] == self.game_board[7] != "[ ]":
+            
+            if self.game_board[1].replace('[','').replace(']','') == self.player_one_sign:
+                print(f"{self.player_one} Wins!!")
+                
+            else:
+                print(f"{self.player_two} Wins!!")
+                
+            return True
+        
+        elif self.game_board[2] == self.game_board[5] == self.game_board[8] != "[ ]":
+            
+            if self.game_board[2].replace('[','').replace(']','') == self.player_one_sign:
+                print(f"{self.player_one} Wins!!")
+                
+            else:
+                print(f"{self.player_two} Wins!!")
+                
+            return True
+        
+        elif self.game_board[0] == self.game_board[4] == self.game_board[8] != "[ ]":
+            
+            if self.game_board[0].replace('[','').replace(']','') == self.player_one_sign:
+                print(f"{self.player_one} Wins!!")
+                
+            else:
+                print(f"{self.player_two} Wins!!")
+                
+            return True
+        
+        elif self.game_board[2] == self.game_board[4] == self.game_board[6] != "[ ]":
+            
+            if self.game_board[2].replace('[','').replace(']','') == self.player_one_sign:
+                print(f"{self.player_one} Wins!!")
+                
+            else:
+                print(f"{self.player_two} Wins!!")
+                
+            return True
+               
+    def start_game(self) -> None:
+          
+        introduction()
+        self.show_board()
+        self.assign_signs_to_players()
+        print(f"{self.player_one} is {self.player_one_sign} and {self.player_two} is {self.player_two_sign}")
+        self.start_match()
+        
+        
+        
+        
+        
+        
+               
+game = Game("Antanas")
+
+game.start_game()
--- game_helpers.py
+++ game_helpers.py
@@ -1,14 +1,5 @@
-
-def show_board():
-    
-    GAME_BOARD = ["[]","[]","[]",
-                  "[]","[]","[]",
-                  "[]","[]","[]"]
-    
-    print(GAME_BOARD[0] + "|" + GAME_BOARD[1] + "|" + GAME_BOARD[2])
-    print(GAME_BOARD[3] + "|" + GAME_BOARD[4] + "|" + GAME_BOARD[5])
-    print(GAME_BOARD[6] + "|" + GAME_BOARD[7] + "|" + GAME_BOARD[8])
-    
 def introduction():
     
-    ...
+    print("Each number corresponds a square")
+    print("To put your sign where you want, type a number that corresponds to the square")
+    print("You must choose an empty square")
